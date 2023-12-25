05efae2496c421065690c1a5442e1087eb1099b6
fffffffee <light_black@126.com>
2023-12-25 13:26:48 +0800

    add Ch4 4.2 ifTest.java

---
+++ 04/4.2/ifTest.java
@@ -0,0 +1,13 @@
+public class ifTest
+{
+	
+	public static void main(String[] args)
+	{
+		var age = 30;
+		if(age > 20)
+		{
+			System.out.println("年龄已经大于20岁了");
+			System.out.println("20岁以上的人应该学会承担责任…");
+		}
+	}
+}
