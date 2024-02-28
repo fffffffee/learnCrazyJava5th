3c01075369499cedcd7912161d29dde82c176264
fffffffee <light_black@126.com>
2024-02-28 13:16:24 +0800

    complete 5.3

---
+++ 05/5.3/BlockTest.java
@@ -0,0 +1,12 @@
+public class BlockTest
+{
+	public static void main(String[] args)
+	{
+		int a;
+		//System.out.println("代码块局部变更a的值：" + a);
+		a = 5;
+		System.out.println("代码块局部变更a的值：" + a);
+	}
+	//System.out.println(a);
+}
+
