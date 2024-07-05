4357089879edee5cb17c2ef2567a6f915c08312f
fffffffee <light_black@126.com>
2024-07-05 23:53:33 +0800

    complete Ch6.7.2

---
+++ 06/6.7/6.7.2/AccessStaticInnerClass.java
@@ -0,0 +1,13 @@
+public class AccessStaticInnerClass
+{
+	static class StaticInnerClass
+	{
+		private static int prop1 = 5;
+		private int prop2 = 9;
+	}
+	public void accessInnerProp()
+	{
+		System.out.println(StaticInnerClass.prop1);
+		System.out.println(new StaticInnerClass().prop2);
+	}
+}
