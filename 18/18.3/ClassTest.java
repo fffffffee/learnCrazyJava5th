3e2233f69c04132c27d243dfda9099480e029471
fffffffee <light_black@126.com>
2025-01-24 23:03:49 +0800

    complete ch18.3

--- 18/18.3/ClassTest.java
+++ 18/18.3/ClassTest.java
@@ -1,4 +1,18 @@
+import java.util.*;
 import java.lang.reflect.*;
+import java.lang.annotation.*;
+@Repeatable(Annos.class)
+@interface Anno {}
+@Retention(value = RetentionPolicy.RUNTIME)
+@interface Annos{
+  Anno[] value();
+}
+// 使用4个注解修饰该类
+@SuppressWarnings(value = "unchecked")
+@Deprecated
+// 使用重复注解修饰该类
+@Anno
+@Anno
 public class ClassTest {
 	private ClassTest()
 	{}
@@ -32,5 +46,27 @@ public class ClassTest {
     for (var md : mtds) {
       System.out.println(md);
     }
+    //获取该Class对象所对应类的指定方法
+    System.out.println("ClassTest里带一个字符串参数的info方法为：" + clazz.getMethod("info", String.class));
+    //获取该Class对象所对应类的全部注解
+    Annotation[] anns = clazz.getAnnotations();
+    System.out.println("ClassTest的全部Annotation如下：");
+    for (var an : anns) {
+      System.out.println(an);
+    }
+    System.out.println("该Class元素上的@SuppressWarnings注解为：" + Arrays.toString(clazz.getAnnotationsByType(SuppressWarnings.class)));
+    System.out.println("该Class元素上的@Anno注解为：" + Arrays.toString(clazz.getAnnotationsByType(Anno.class)));
+    //获取该Class对象所对应类的全部内部类
+    Class<?>[] inners = clazz.getDeclaredClasses();
+    System.out.println("ClassTest的全部内部类如下：");
+    for (var c : inners) {
+      System.out.println(c);
+    }
+    // 使用Class.forName()方法加载ClassTest的Inner内部类
+    Class inClazz = Class.forName("ClassTest$Inner");
+    // 通过getDeclaringClass()访问该类所在的外部类
+    System.out.println("inClazz对应该类所在的外部类为：" + inClazz.getDeclaringClass());
+    System.out.println("ClassTest的包为：" + clazz.getPackage());
+    System.out.println("ClassTest的父类为：" + clazz.getSuperclass());
   }
 }
